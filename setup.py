9f055a3a13784b7949d8d1bb223e9573d911a9c9
Roy Hooper <rhooper@toybox.ca>
2021-03-20 19:07:34 -0400

    prepare for pypi

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pelican-lunr",
+    version="0.2.1",
+    author="R Hooper",
+    author_email="rhooper@toybox.ca",
+    description="A plugin that provides lunr index creation for the Pelican static site generator.",
+    keywords='pelican lunr search plugin',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url="https://github.com/kattni/pelican-lunr",
+    project_urls={
+        "Bug Tracker": "https://github.com/kattni/pelican-lunr/issues",
+        "Source": "https://github.com/kattni/pelican-lunr/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['pelican_lunr'],
+    python_requires=">=3.6",
+    install_requires=[
+        'beautifulsoup4',
+        'pelican>4.5.0,<5',
+        'lunr'
+    ]
+)
---
+++ tests/test_pelican_lunr.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""Basic test for pelican_lunr package."""
+
+import pytest
+
+
+class TestPelicanLunr:
+    def test_import(self):
+        import pelican_lunr
+
